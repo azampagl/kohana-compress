7a16ee956f3354a124594d27ca3798fe26369073
Aaron Zampaglione <azampagl@azampagl.com>
2011-02-28 13:28:39 -0500

    Updating Closure Service class to use new 3.1 Request call.

--- classes/compress/compressor/closure/service/core.php
+++ classes/compress/compressor/closure/service/core.php
@@ -25,20 +25,17 @@ abstract class Compress_Compressor_Closure_Service_Core extends Compress_Compres
 		}
 
 		// HTTP query
-		$query = array(
+		$post = array(
 			'code_url'			=> $files,
 			'compilation_level'	=> $this->_config['compilation_level'],
 			'output_format'		=> 'text',
 			'output_info'		=> 'compiled_code',
 		);
 
-		// Play nice with HTTP query arrays
-		$post = preg_replace('/%5B(?:[0-9]|[1-9][0-9]+)%5D=/', '=', http_build_query($query));
-
-		$response = Remote::get($this->_config['url'], array(
-			CURLOPT_POST       => TRUE,
-			CURLOPT_POSTFIELDS => $post,
-		));
+		$response = Request::factory($this->_config['url'])
+			->method(HTTP_Request::POST)
+			->post($post)
+			->execute();
 
 		file_put_contents($out, $response);
 	}
