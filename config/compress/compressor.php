2dc880d3794d244f3b24c08cc9ab4529f07f2167
Aaron Zampaglione <azampagl@azampagl.com>
2013-08-17 09:23:23 -0400

    Upgraded YUI to 2.4.8.

--- config/compress/compressor.php
+++ config/compress/compressor.php
@@ -5,7 +5,7 @@ return array
 	'yui'					=> array
 	(
 		'java'				=> 'java',
-		'jar'				=> 'vendor/yui/yuicompressor-2.4.6.jar',
+		'jar'				=> 'vendor/yui/yuicompressor-2.4.8.jar',
 	),
 	'closure_application'	=> array
 	(
